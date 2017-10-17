798f857b9e8f602956d2e2ed494cc011a26b2e3d
Jan Erik Hanssen <jhanssen@gmail.com>
2017-10-17 03:00:32 -0700

    repeat

--- test/test.js
+++ test/test.js
@@ -7,19 +7,25 @@ const Speaker = require("speaker");
 
 //console.log(FlacDecoder);
 
-function wrapSpeaker(speaker) {
-    speaker.removeListener('finish', speaker._flush);
-    speaker._flush = () => {
-        speaker.emit('flush');
-        setTimeout(() => {
-            speaker.close(true);
-        }, 500);
-    };
-    speaker.on('finish', speaker._flush);
-    return speaker;
+function play() {
+    function wrapSpeaker(speaker) {
+        speaker.removeListener('finish', speaker._flush);
+        speaker._flush = () => {
+            speaker.emit('flush');
+            setTimeout(() => {
+                speaker.close(true);
+
+                process.nextTick(play);
+            }, 500);
+        };
+        speaker.on('finish', speaker._flush);
+        return speaker;
+    }
+
+    createReadStream("/tmp/test.flac")
+        .pipe(new FlacDecoder)
+        .on("format", console.log)
+        .pipe(wrapSpeaker(new Speaker));
 }
 
-createReadStream("/tmp/test.flac")
-    .pipe(new FlacDecoder)
-    .on("format", console.log)
-    .pipe(wrapSpeaker(new Speaker));
+play();
